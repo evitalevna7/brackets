6149f44016c16cb706255e0bfcba9e9c9d124316
namihaha77 <47521419+namihaha77@users.noreply.github.com>
2020-03-02 20:01:31 +0400

    feat: attempt to solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let circle = 0;
+  let square = 0;
+  let curly = 0;
+  
+    for (const item of str) {
+        if (circle > 0 || square > 0 || curly > 0) {
+          return false;
+        }
+        if (item === '(') {
+          circle -= 1;
+        }
+        if (item === ')') {
+          circle += 1;
+        }
+        if (item === '[') {
+          square -= 1;
+        }
+        if (item === ']') {
+          square += 1;
+        }
+        if (item === '{') {
+          curly -= 1;
+        }
+        if (item === '}') {
+          curly += 1;
+        }
+    }
+  
+  return true;
 }
